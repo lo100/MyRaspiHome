7806331ba5a0f74a4672877e238beceeb201673b
hari01be <bernhard.hari@ruag.com>
2014-07-21 20:26:11 +0000

    Initial add.

---
+++ data_interpolator/data_interpolator.py
@@ -0,0 +1,17 @@
+'''
+Created on 06.03.2014
+
+@author: harb
+'''
+
+class DataInterpolator(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ tests/data_cleanser/data_cleanser_test.py
@@ -0,0 +1,26 @@
+'''
+Created on 06.03.2014
+
+@author: harb
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
