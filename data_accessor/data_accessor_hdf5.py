2b39a20fbdae1efb09a10c034965674cb193f2e4
hari01be <bernhard.hari@ruag.com>
2014-07-21 20:26:52 +0000

    Implemented data accessor for HDF5 files.

---
+++ data_accessor/data_accessor_hdf5.py
@@ -0,0 +1,149 @@
+"""
+   module:: data_accessor
+   :synopsis: Data accessor for HDF5 files.
+   
+   moduleauthor:: Bernhard Hari <github@taschenwerkstatt.ch>
+"""
+import h5py
+import numpy as np
+
+from data_accessor.exceptions import DataAccessorError
+
+class DataAccessorHDF5():
+
+    """The HDF5 file.
+    """
+    _file = None
+
+    """Data accessor type.
+    """
+    DATA_ACCESSOR_TYPE = 'hdf5'
+
+    def __init__(self):
+        pass
+
+    def _create_root_node(self):
+        # create root node if not exist
+        root_node = 'series'
+        if root_node not in self._file['/'].keys():
+                self._file.create_group(root_node)
+        return '/' + root_node
+    
+    def _create_source_node(self, source=None):
+        # create source node if not exist
+        root_node = self._create_root_node()
+        source_node = root_node
+        if source != None:
+            source_node += '/' + source
+            if source not in self._file[root_node].keys():
+                self._file.create_group(source_node)
+        return source_node
+            
+    def open(self, filename, mode='a'):
+        """ Opens the hdf5 file used by the data accessor.
+        
+        :param filename: Name of the file to be opened (including path).
+        :type filename: string
+        :param mode: File open mode.
+        :type mode: string
+        :rtype: boolean
+        """
+        try:
+            self._file = h5py.File(filename, mode)
+        except:
+            raise DataAccessorError('HDF5 file could not be opened! ' + filename)
+
+    def close(self):
+        """ Closes the HDF5 file.
+        
+        :rtype: boolean
+        """
+        self._file.close()
+        return True
+
+    def read(self, source=None, parameter=(), offset=None, length=None):
+        """Reads from the HDF5 file.
+        
+        :param source: The data source (device).
+        :type source: string
+        :param parameter: A list of parameter to be read.
+        :type parameter: string list
+        :param offset: offset to start reading data.
+        :type offset: integer
+        :param length: max number off values per parameter to be read.
+        :type length: integer       
+        :rtype: dictionary
+        """
+
+        # TODO slicing and check offset, length
+        data_dictionary = {}
+        for param in parameter:
+            source_node = self._create_source_node(source)
+            node = source_node + '/' + param
+
+            # check if parameter exists
+            if param in self._file[source_node].keys():
+                data = self._file[node]['data']
+                # TODO length
+                data_mask = self._file[node]['mask']
+                values = np.ma.masked_array(data, mask=data_mask)
+                data_dictionary[param] = values
+            elif param in self._file[source_node].attrs:
+                data_dictionary[param] = self._file[source_node].attrs[param]
+        return data_dictionary
+
+    def write(self, source=None, data=[], configuration=None, offset=0, initial_size=1440):
+        """Writes data to HDF5 file.
+        
+        :param source: The data source (device).
+        :type source: string
+        :param data: A list of recorded data (one dimensional dictionary) to be written.
+        :type data: string list
+        :param configuration: A two-dimensional dictionary for data configuration e.g. {'paramA':{type':'int8', 'attribute':False},...}
+        :type configuration: dictionary 
+        :param offset: offset to start writing data
+        :type offset: integer
+        :param initial_size: number of samples per day, the expected initial dataset size
+        :type initial_size: integer
+        :rtype: integer
+        """
+        
+        # create source node if not exist
+        source_node = self._create_source_node(source)
+        
+        # saving values to dataset
+        for recording in data:
+            for param, value in recording.items():
+                
+                node = source_node + '/' + param
+                data_node = node + '/data'
+                mask_node = node + '/mask'
+                
+                # write attribute
+                # ---------------
+                if configuration[param]['attribute'] == True:
+                    # add attribute if not already exists
+                    if param not in self._file[source_node].attrs:
+                        self._file[source_node].attrs[param] = value
+                        
+                # write parameter
+                # ---------------
+                elif configuration[param]['attribute'] == False:
+                    dataset_values = None
+                    dataset_masked = None
+                
+                    if param not in self._file[source_node].keys():
+                        dataset_values = self._file.create_dataset(data_node, (initial_size,), dtype=configuration[param]['type'])
+                        dataset_masked = self._file.create_dataset(mask_node, (initial_size, 1), dtype=np.int8)
+                    else:
+                        dataset_values = self._file[data_node]
+                        dataset_masked = self._file[mask_node]
+                    
+                    if value != None:
+                        dataset_values[offset] = value
+                        dataset_masked[offset] = 0
+                # else: attribute == None
+                 
+            offset += 1
+        return True
+    
---
+++ tests/data_accessor/data_accessor_hdf5_test.py
@@ -0,0 +1,67 @@
+import unittest
+import sys
+import logging
+import numpy as np
+
+from data_accessor.data_accessor_hdf5 import DataAccessorHDF5
+# from data_accessor.exceptions import DataAccessorError
+
+class TestDataAccessorHDF5(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger()
+        self.logger.level = logging.DEBUG
+        self.stream_handler = logging.StreamHandler(sys.stdout)
+        self.logger.addHandler(self.stream_handler)
+
+    def test_new_data_accessor_hdf5(self):
+        self.logger.info('test_new_data_accessor_hdf5: Create new data accessor HDF5.')
+        da = DataAccessorHDF5()
+        self.assertEqual(da.DATA_ACCESSOR_TYPE, 'hdf5')
+
+    def test_write_read_hdf5(self):
+        self.logger.info('test_write_read_hdf5: Open HDF5 file and write/read data to/from file.')
+        da = DataAccessorHDF5()
+        
+        # write data to file
+        da.open('E:\\wa_git\\output\\unittest.hdf5', 'w')
+        configuration = {'float':{'type':'float32', 'attribute':False},
+                        'integer':{'type':'int16', 'attribute':False},
+                        'string':{'type':'S3', 'attribute':False},
+                        'attr1':{'type':'int16', 'attribute':True},
+                        'attr2':{'type':'S5', 'attribute':True}}
+        recordings = []
+        recordings.append({'float' : 0.1234,
+                    'integer' : 1234,
+                    'string' : 'abc',
+                    'attr1' : '1234',
+                    'attr2' : 'rec01',
+                    })
+        recordings.append({'float' : 0.5678,
+                    'integer' : 5678,
+                    'string' : 'def',
+                    'attr1' : '2345',
+                    'attr2' : 'rec02',
+                    })
+        
+        da.write('unittest', recordings, configuration)
+        da.close()
+        
+        # read data from file
+        da.open('E:\\wa_git\\output\\unittest.hdf5', 'r')
+        parameter = ('float', 'integer', 'string', 'attr1', 'attr2')
+        data = da.read('unittest', parameter)
+        self.assertEqual(data['float'][0], np.float32(recordings[0]['float']))
+        self.assertEqual(data['integer'][0], recordings[0]['integer'])
+        self.assertEqual(data['string'][0], recordings[0]['string'])
+        self.assertEqual(data['float'][1], np.float32(recordings[1]['float']))
+        self.assertEqual(data['integer'][1], recordings[1]['integer'])
+        self.assertEqual(data['string'][1], recordings[1]['string'])        
+        self.assertEqual(data['attr1'], recordings[0]['attr1'])
+        self.assertEqual(data['attr2'], recordings[0]['attr2'])
+        da.close()
+                
+        
+    def tearDown(self):
+        self.logger.removeHandler(self.stream_handler)
+        self.stream_handler.close()
+
