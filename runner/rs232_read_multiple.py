d9c1b6d6bfbd54e1285cc94923cfab58bb2b16e9
lo100 <bernhard.hari@taschenwerkstatt.ch>
2014-12-27 20:55:46 +0000

    Added time to logfile, fixed code according to pylint messages

--- runner/rs232_read_multiple.py
+++ runner/rs232_read_multiple.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-
 
-#
-# python sample application that reads serial data from RX/TX using the GPIO ports
-#
+"""
+module:: runner
+:synopsis: Application that reads data from Steca Tarom solar charger using
+the RS-232 interface.
+
+moduleauthor:: Bernhard Hari <github@taschenwerkstatt.ch>
+
+License: MIT (http://opensource.org/licenses/MIT)
+
+Start the script:
+$ python rs232_read_multiple.py
+"""
 
 import array
 import serial
@@ -12,10 +22,10 @@ from time import gmtime, strftime
 maxReadCount = 105
 readBuffer = array.array('c')
 
-print('this sample application reads from the rs-232 port')
+print 'Connect to Steca Tarom solar charger'
 
 # open the port
-print('opening device /dev/ttyAMA0')
+print 'Opening device /dev/ttyAMA0'
 try:
     ser = serial.Serial(port='/dev/ttyAMA0', baudrate=4800)
     ser.parity = 'N'
@@ -23,23 +33,22 @@ try:
     ser.bytesize = 8
     ser.flowcontrol = 'off'
 except:
-    print('failed.')
-    print('possible causes:')
-    print('1) the raspicomm device driver is not loaded. type \'lsmod\' and verify that you \'raspicommrs232\' is loaded.')
-    print('2) the raspicomm device driver is in use. Is another application using the device driver?')
-    print('3) something went wrong when loading the device driver. type \'dmesg\' and check the kernel messages')
+    print 'ERROR could not connect to Steca Tarom solar charger!'
+    print 'Possible causes:'
+    print '1) the serial device is in use. Is another application using the device ttyAMA0?'
+    print '2) something went wrong when loading the device driver. type \'dmesg\' and check the kernel messages'
     exit()
 
-print('successful.')
+print 'successful.'
 
-filename = '/var/log/solar/log_' + strftime("%Y%m%d", gmtime()) + '.txt'
+filename = '/var/log/solar/log_' + strftime("%Y%m%d_%H%M%S", gmtime()) + '.txt'
 logfile = file(filename, 'w')
 
 # print the received bytes
-print('we received the following bytes:')
+print 'INFO start reading data from Steca Tarom solar charger'
 while True:
     data = ser.readline()
-    print(data)
+    # print data
     logfile.write(data)
     logfile.flush()
 logfile.close()
